da6a4188b0494fa47647bab1670c97d772749c18
Mariia <mouse250396@gmail.com>
2021-08-28 16:46:27 +0300

    some changes

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,1 +1,29 @@
+function clickOnBrowseButton(){
+    document.querySelector('.search-form__browse')
+            .addEventListener('click', () => {
+                if (document.querySelector('.search-form__browse')
+                .nextElementSibling.classList.contains('drop__down')){
+                    document.querySelector('.search-form__browse')
+                .nextElementSibling.classList.toggle('drop__down');
+                }
+                else {
+                    document.querySelector('.search-form__browse')
+                .nextElementSibling.classList.add('drop__down');
+                }
+    });
+}
 
+
+function makeActiveNavItem(){
+    let pageId = document.querySelector("[data-id-page]").getAttribute("data-id-page"),
+        navItem = document.querySelector(`[data-id-nav=${pageId}]`);
+
+    if(pageId === navItem.getAttribute("data-id-nav")) {
+        navItem.classList.add("menu__list_active");
+    }
+}
+
+
+
+clickOnBrowseButton();
+makeActiveNavItem();
